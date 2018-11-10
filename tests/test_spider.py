4cf994168469f3d70b94e44689a753d84126f660
callumski <callum@cmdatasol.com>
2018-11-10 21:38:08 +0100

    split links to internal and external lists

--- tests/test_spider.py
+++ tests/test_spider.py
@@ -2,8 +2,8 @@ import os
 
 from scrapy.http import TextResponse, Request
 
-from ..spiders.spider import CrawlitSpider
-from ..items import CrawlitItem
+from crawlit.spiders.spider import CrawlitSpider
+from crawlit.items import CrawlitItem
 
 
 def mock_response_from_file(filename, url = None):
@@ -33,13 +33,14 @@ def mock_response_from_file(filename, url = None):
 def test_linkless_page():
     spider = CrawlitSpider()
     items = list(spider.parse(mock_response_from_file("linkless.html")))
-    assert len(items[0]["links"]) == 0
+    assert len(items[0]["internal_links"]) == 0
+    assert len(items[0]["external_links"]) == 0
 
 def test_oneimage_page():
     spider = CrawlitSpider()
     items = list(spider.parse(mock_response_from_file("oneimage.html")))
-    links = items[0]["links"]
-    assert len(links) == 0
+    assert len(items[0]["internal_links"]) == 0
+    assert len(items[0]["external_links"]) == 0
     images = items[0]["static_content"]
     assert len(images) == 1
     assert images[0] == "monalisa.jpg"
@@ -48,17 +49,18 @@ def test_one_internal_link_page():
     spider = CrawlitSpider()
     spider.allowed_domains=["http://www.example.com"]
     results = list(spider.parse(mock_response_from_file("oneinternal.html")))
-    print (results)
-    assert isinstance(results[0], Request)
-    assert isinstance(results[1], CrawlitItem)
-    links = results[1]["links"]
-    assert len(links) == 1
-    assert links[0] == "about_us.html"
+    assert isinstance(results[0], CrawlitItem)
+    assert len(results[0]["external_links"]) == 0
+    internal_links = results[0]["internal_links"]
+    assert internal_links == ["about_us.html"]
+    assert isinstance(results[1], Request)
+
 
 def test_one_external_link_page():
     spider = CrawlitSpider()
     results = list(spider.parse(mock_response_from_file("oneexternal.html")))
+    assert len(results) == 1
     assert isinstance(results[0], CrawlitItem)
-    links = results[0]["links"]
-    assert len(links) == 1
-    assert links[0] == "http://www.example.com/interesting_page.html"
+    external_links = results[0]["external_links"]
+    assert external_links == ["http://www.example.com/interesting_page.html"]
+    assert len(results[0]["internal_links"]) == 0
