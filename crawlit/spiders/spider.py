f1396da0e15135af31267db4fef48bb6af5306ac
callumski <callum@cmdatasol.com>
2018-11-10 21:34:37 +0100

    split links to internal and external lists

--- crawlit/spiders/spider.py
+++ crawlit/spiders/spider.py
@@ -5,7 +5,15 @@ from ..items import CrawlitItem
 
 
 class CrawlitSpider(Spider):
+    """
+    Simple web crawler, limited to one domain. Given a starting URL it will visit all pages within the domain, but not
+    follow the links to external sites such as Google or Twitter.
+
+    It collects a list of items with url, internal links, external links and static content.
+    """
+
     name = 'spider'
+    allowed_domains = []
 
     def start_requests(self):
         url = getattr(self, 'url', None)
@@ -16,13 +24,24 @@ class CrawlitSpider(Spider):
             yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
-        links = []
-        for url in response.xpath('//a/@href').extract():
-            links.append(url)
+        internal_links = []
+        external_links = []
 
+        urls = response.xpath('//a/@href').extract()
+
+        for url in urls:
+            netloc = urlparse(url).netloc
+            if netloc == '' or netloc in self.allowed_domains:
+                internal_links.append(url)
+            else:
+                external_links.append(url)
 
         item = CrawlitItem()
         item['url'] = response.url
         item['static_content'] = response.xpath('//*[contains(@src, ".")]').xpath('@src').extract()
-        item['links'] = links
+        item['internal_links'] = internal_links
+        item['external_links'] = external_links
         yield item
+
+        for url in internal_links:
+            yield response.follow(url, callback=self.parse)
