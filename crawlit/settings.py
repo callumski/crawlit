44abd3451af248761bc253706e0431e7d18433db
callumski <callum@cmdatasol.com>
2018-11-10 22:55:09 +0100

    applied scrapy autothrottle

--- crawlit/settings.py
+++ crawlit/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'crawlit.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'crawlit (+http://www.yourdomain.com)'
+USER_AGENT = 'crawlit (http://www.cmdatasol.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -70,11 +70,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
